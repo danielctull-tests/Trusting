3b6562711c5939f893c21cc28f7fd695f624279c
Daniel Tull <dt@danieltull.co.uk>
2017-02-23 13:16:34 +0000

    Initial test to trust self-signed CA

--- Trusting/ViewController.swift
+++ Trusting/ViewController.swift
@@ -1,25 +1,61 @@
-//
-//  ViewController.swift
-//  Trusting
-//
-//  Created by Daniel Tull on 23.02.2017.
-//  Copyright © 2017 Daniel Tull. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDelegate {
 
 	override func viewDidLoad() {
+
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		guard
+//			let certificateURL = Bundle(for: ViewController.self).url(forResource: "wireless.danieltull.co.uk", withExtension: "cer"),
+			let certificateURL = Bundle(for: ViewController.self).url(forResource: "Daniel Tull CA 2017", withExtension: "cer"),
+			let certificateData = try? Data(contentsOf: certificateURL),
+			let certificate = SecCertificateCreateWithData(nil, certificateData as CFData)
+		else {
+			return
+		}
+
+		var secTrust: SecTrust?
+		let policy = SecPolicyCreateSSL(true, "wireless.danieltull.co.uk" as CFString)
+		SecTrustCreateWithCertificates(certificate, policy, &secTrust)
+
+		guard let trust = secTrust else {
+			return
+		}
+
+		SecTrustSetAnchorCertificates(trust, [certificate] as CFArray)
+//		SecTrustSetAnchorCertificatesOnly(trust, false)
+
+		let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: OperationQueue.main)
+		let url = URL(string: "https://wireless.danieltull.co.uk/api/status.json")!
+		let task = session.dataTask(with: url) { (data, response, error) in
+
+			if let data = data {
+				print("Data: ", data)
+			}
+
+			if let error = error {
+				print("Error: ", error)
+			}
+
+			if let response = response {
+				print("Response: ", response)
+			}
+		}
+
+		task.resume()
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+
+		guard let trust = challenge.protectionSpace.serverTrust else {
+			print("FAILED")
+			completionHandler(.cancelAuthenticationChallenge, nil)
+			return
+		}
+
+		let credential = URLCredential(trust: trust)
+		completionHandler(.useCredential, credential)
 	}
-
-
 }
-
